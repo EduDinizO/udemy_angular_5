535d6716729a78c04f11da26e30260e057736a89
EduDinizO <edu_diniz@hotmail.com>
2018-02-25 13:02:41 -0300

    ingredient model fix

--- src/app/models/ingredient.model.ts
+++ src/app/models/ingredient.model.ts
@@ -1,3 +1,9 @@
 export class Ingredient {
-    constructor(name: string, amount: number) { }
+    public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }
 }
