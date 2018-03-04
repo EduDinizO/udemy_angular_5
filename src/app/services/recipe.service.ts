bf7fcac45364f2bec46d78c858b44b0cb6244f7a
EduDinizO <edu_diniz@hotmail.com>
2018-03-04 16:38:43 -0300

    add ingredient to recipe model

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,11 +1,27 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { Recipe } from '../models';
+import { Recipe, Ingredient } from '../models';
 
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('Sopa', 'Caldo', 'https://t2.rg.ltmcdn.com/pt/images/2/6/4/img_sopa_de_mandioca_462_600.jpg'),
-        new Recipe('Macarrão', 'Massa', 'https://img.cybercook.uol.com.br/imagens/receitas/406/massa-de-macarrao.jpg'),
+        new Recipe(
+            'Sopa',
+            'Para tomar no frio',
+            'https://t2.rg.ltmcdn.com/pt/images/2/6/4/img_sopa_de_mandioca_462_600.jpg',
+            [
+                new Ingredient('Água', 1),
+                new Ingredient('Ervilha', 5),
+            ],
+        ),
+        new Recipe(
+            'Macarrão',
+            'Vem da Itália',
+            'https://img.cybercook.uol.com.br/imagens/receitas/406/massa-de-macarrao.jpg',
+            [
+                new Ingredient('Massa', 1),
+                new Ingredient('Molho', 2),
+            ],
+        ),
     ];
 
     public recipeSelected = new EventEmitter<Recipe>();
